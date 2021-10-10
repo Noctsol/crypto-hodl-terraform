38a7ae36931ca1758e020758b3dbeaca725dd603
Noctsol <39346808+Noctsol@users.noreply.github.com>
2021-10-09 21:54:52 -0700

    KB - Added initial infra code

--- main.tf
+++ main.tf
@@ -1,12 +1,26 @@
 
+# Creating a Project (think Resource Group/Subscription from Azure)
+resource "digitalocean_project" "proj" {
+  name        = "crypto-hodl"
+  description = "Holds all the infrastructure resources for crypto-hodl to analyze crypto assets"
+  purpose     = "Analytics"
+  environment = "Production"
+}
 
-
-# Generate
-resource "digitalocean_database_cluster" "mongodb-example" {
-  name       = "example-mongo-cluster"
-  engine     = "mongodb"
-  version    = "4"
+# Generate # DB
+resource "digitalocean_database_cluster" "crypto_db" {
+  name       = local.ch_db_name
+  engine     = var.db_engine
+  version    = "4.4"
   size       = "db-s-1vcpu-1gb"
-  region     = "nyc3"
+  region     = var.city_zone
   node_count = 1
+}
+
+# Add resource to proj
+resource "digitalocean_project_resources" "proj_resources" {
+  project = digitalocean_project.proj.id
+  resources = [
+    digitalocean_database_cluster.crypto_db.urn
+  ]
 }
--- variable.tf
+++ variable.tf
@@ -1,6 +1,45 @@
+variable "city_zone"{
+    type = string
+    description = "Specifies the city and zone the resource will be deployed to"
+    default = "sfo3"
+
+    validation {
+      condition = contains(["sfo1", "sfo2", "sfo3", "nyc1", "nyc1"," nyc3"], var.city_zone)
+      error_message = "The city_zone value must be in sfo1, sfo2, sfo3, nyc1, nyc1, or nyc3."
+    }
+}
+
+variable "project_name" {
+    type = string
+    description = "Name of the project"
+    default = "cryptohodl"
+}
+
+variable "db_engine" {
+    type = string
+    description = "Type of databse getting generated"
+    default = "mongodb"
+
+    validation {
+      condition = contains(["pg", "mysql", "redis", "mongodb"], var.db_engine)
+      error_message = "The db_engine value must be in pg (PostreSQL), mysql (MySQL), redis (REDIS) , or mongodb (MongoDB)."
+    }
+}
+
+# variable "city_zone"{
+#     type = string
+#     description = "Specifies the city and zone the resource will be deployed to"
+#     default = "sfo3"
+#     validation {
+#       condition = contains(["sfo1", "sfo2", "sfo3", "nyc1", "nyc1"," nyc3"], var.city_zone)
+#       error_message = "city_zone value must be in sfo1, sfo2, sfo3, nyc1, nyc1, nyc3"
+#     }
+# }
+
+locals {
+  ch_db_name = "${var.db_engine}_${var.city_zone}_${var.project_name}"
+}
 
 
-
-
-# Variables inherited from terraform cloud
+# Variables inherited from terraform cloud (secrets)
 variable "do_token" {}
